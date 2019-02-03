6cca9f6c24c21243a35a5e5a007a1d3636de95f5
actionpterygii <actionpterygii@gmail.com>
2019-02-04 00:12:44 +0900

    indento

--- kaijo.scm
+++ kaijo.scm
@@ -1,9 +1,12 @@
 (define (main args)
-  (print (factorial　(x->integer (cadr args))  1))
+  (print
+    (factorial (x->integer (cadr args)) 1)
+  )
 0)
 
 (define (factorial n s)
-  (if (= n 0) s
-    (factorial (- n 1)(* n s))
+  (if (= n 0)
+    s
+    (factorial (- n 1) (* n s))
   )
 )
